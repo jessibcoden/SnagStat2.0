8d308b5dcecb7573e85a1b9d22c70eb91d7ab262
Jess <jessibcoden@gmail.com>
2018-06-02 12:59:53 -0500

    WIP

--- SsApi/Migrations/Configuration.cs
+++ SsApi/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace SsApi.Migrations
 {
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using SsApi.Models;
+    using SsApi.Models.DataModels;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,6 +17,30 @@ namespace SsApi.Migrations
 
         protected override void Seed(SsApi.Models.ApplicationDbContext context)
         {
+            var adminRole = new IdentityRole("Admin");
+            context.Roles.AddOrUpdate(r => r.Name, adminRole);
+
+            var trackerRole = new IdentityRole("Tracker");
+            context.Roles.AddOrUpdate(r => r.Name, trackerRole);
+
+            context.SaveChanges();
+
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var user = new ApplicationUser
+            {
+                UserName = "nathan",
+                Email = "nathan@copypastedeveloper.com",
+                IsCoach = true
+            };
+
+            userManager.CreateAsync(user, "password").Wait();
+
+            var addedUser = context.Users.First(x => x.UserName == user.UserName);
+
+            userManager.AddToRoleAsync(addedUser.Id, "Admin").Wait();
+
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
